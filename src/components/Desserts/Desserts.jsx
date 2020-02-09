6b7335d42432ae7094da058e53203b5f0bce2eb0
crr <crr@example.com>
2020-02-09 18:18:36 +0100

    FIX - render list desserts && styling desserts grid

--- src/components/Desserts/Desserts.jsx
+++ src/components/Desserts/Desserts.jsx
@@ -1,102 +1,60 @@
 import React, { useState } from "react";
 import dessertsDataFromJson from "./dessertsdata.json";
 
-import "./Desserts.css";
+import Dessert from "../Dessert/Dessert.jsx";
 
 const Desserts = () => {
-  // const [count, setCount] = useState(0);
-  // const [itemchanged, setItemChanged] = useState(false)
   const [dessertsData, setDessertsData] = useState(dessertsDataFromJson);
 
-  // useEffect(() => {
-  //   console.log("desserts before sorting", dessertsData)
-  //   const copyDesserts = [...dessertsData]
-  //   const dessertsSorted = copyDesserts.sort((a, b) => {
-  //     if (a.rate < b.rate) return 1;
-  //     if (a.rate > b.rate) return -1;
-  //     return 0;
-  //   });
-  //   console.log("deserts after sorting", dessertsSorted)
-  //   setDessertsData(dessertsSorted);
-  //   // getDesserts();
-  //   // console.log(dessertsSorted);
-  //   console.log("updated!")
-  // }, []);
-  const sortAndUpdateDesserts = (desserts) => {
+  const sortAndUpdateDesserts = desserts => {
     const dessertsSorted = desserts.sort((a, b) => {
       if (a.rate < b.rate) return 1;
       if (a.rate > b.rate) return -1;
+      if (a.rate === b.rate) {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      }
       return 0;
     });
-    setDessertsData(dessertsSorted)
-  }
+    setDessertsData(dessertsSorted);
+  };
 
   const addRateHandler = event => {
-    // Copiar objecte desertsData
-    const desserts = [...dessertsData]
-    console.log("desserts", desserts)
-    // Identificar quin desert he clicat i canviar-ho a dins de l'objecte COPIAT de desertsData
-    const selectedDessert = event.target.value
+    const desserts = [...dessertsData];
+    const selectedDessert = event.target.value;
     const updatedDesserts = desserts.map(dessert => {
-      if(dessert.id === selectedDessert) {
-        dessert.rate += 1
+      if (dessert.id === selectedDessert) {
+        dessert.rate += 1;
       }
-      return dessert
-    })
-    sortAndUpdateDesserts(updatedDesserts)
+      return dessert;
+    });
+    sortAndUpdateDesserts(updatedDesserts);
   };
 
   const subtractRateHandler = event => {
-    const dessertId = event.target.value;
-    const dessertSelected = dessertsData.filter(selected => {
-      return selected.id === dessertId;
+    const desserts = [...dessertsData];
+    const selectedDessert = event.target.value;
+    const updatedDesserts = desserts.map(dessert => {
+      if (dessert.id === selectedDessert) {
+        dessert.rate -= 1;
+      }
+      return dessert;
     });
-    dessertSelected[0].rate -= 1;
+    sortAndUpdateDesserts(updatedDesserts);
   };
 
   const getDesserts = () => {
-    console.log(dessertsData);
     return (
-      <ul>
-        {dessertsData.map(dessert => (
-          <li className="dessert" key={dessert.id}>
-            <img
-              className="dessert-image"
-              src={dessert.image}
-              alt={dessert.title}
-            />
-            <div className="dessert-information">
-              <span className="dessert-information__title">
-                {dessert.title}
-              </span>
-              <span className="dessert-information__rate">
-                Rate: {dessert.rate}
-              </span>
-              <div className="dessert-information__votes">
-                <button onClick={addRateHandler} value={dessert.id}>
-                  {" "}
-                  <i className="far fa-thumbs-up"></i>
-                  Add vote!
-                </button>
-                <button
-                  disabled={dessert.rate <= 0}
-                  onClick={subtractRateHandler}
-                  value={dessert.id}
-                >
-                  <i className="far fa-thumbs-down"></i>
-                  Remove vote!
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Dessert
+        dessertsData={dessertsData}
+        onClickHandler={subtractRateHandler}
+        onClickedHandler={addRateHandler}
+      ></Dessert>
     );
   };
-  
+
   return <div>{getDesserts()}</div>;
 };
 
 export default Desserts;
-
-// https://www.taste.com.au/recipes/collections/top-50-desserts?page=2&q=&sort=recent
